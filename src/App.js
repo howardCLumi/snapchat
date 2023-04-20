c3262c128c96e51a0da2cbb0baae22b99a253a2a
howardCLumi <102765211+howardCLumi@users.noreply.github.com>
2023-04-20 13:52:48 -0500

    created header and body + multiple pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      HI SNAPCHAT
+    <div className="app">
+      <Header />
+      <Routes>
+      <Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/contact' element={<Contact/>}></Route>
+    </Routes>
+    <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
