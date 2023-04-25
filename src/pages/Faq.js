e7857117eb2e66ed50ac942fdea1d366c8c6ccf1
howardCLumi <102765211+howardCLumi@users.noreply.github.com>
2023-04-24 23:23:23 -0500

    fixed Q&A page format. looks nice now!!

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -144,66 +144,74 @@ const Faq = () => {
       <div className="faq__parallax">
         <div className="faq__header">
           <a href="#form-questions">Form Questions</a>
-          <a href="#resume-faq">Resume</a>
+          <a href="#resume">Resume</a>
           <a href="#cover-letter">Cover Letter</a>
         </div>
       </div>
       <div className="faq">
         <div className="faq__container">
-          <h2 id="form-questions" className="faq__form">
-            Form Questions
-          </h2>
-          {promptsData.map((prompt) => (
-            <>
-              <button className="collapsible">{prompt.question}</button>
-              <div className="faq__prompt-question">
-                <p>{prompt.answer}</p>
-              </div>
-            </>
-          ))}
-          <h2 id="resume" className="faq__resume-title">
-            Resume
-          </h2>
-          <div className="faq__resume-container">
-            <img src={resume} className="faq__resume" alt="resume"></img>
+          <div className="faq__form-real-container">
+            <h2 id="form-questions" className="faq__form">
+              Form Questions
+            </h2>
+            {promptsData.map((prompt) => (
+              <>
+                <button className="collapsible">{prompt.question}</button>
+                <div className="faq__prompt-question">
+                  <p>{prompt.answer}</p>
+                </div>
+              </>
+            ))}
           </div>
-          <h2 id="cover-letter" className="faq__cover-letter">
-            Cover Letter
-          </h2>
-          <div className="faq__cover-letter-text">
-            <p>Dear Snap Engineering Academy,</p>
-            <p>
-              I am excited to submit my Website and potentionally move on to the
-              final round for a position at Snap Engineering Academy. As a
-              transfer student in Software Engineering, I have tried my best to
-              hone my skills in software engineering, programming, and data
-              analysis through hands-on experience and academic coursework. My
-              passion for innovation and technology has driven me to explore the
-              latest advancements in the field and learn new technologies to
-              stay current in this ever-evolving industry.
-            </p>
-            <p>
-              My previous work experience as a software engineer at John Deere
-              has given me the opportunity to develop and deliver successful
-              software solutions. I have gained proficiency in programming
-              languages such as Python, Java, and Web Developing languages. I
-              have also gained experience in agile development methodologies and
-              understand the importance of teamwork, communication, and
-              collaboration in achieving project goals.
-            </p>
-            <p>
-              I am drawn to Snapchat's commitment to creativity, innovation, and
-              the company's unique culture. I am excited to join the Snap
-              Engineering Academy team and contribute to developing high-quality
-              software solutions that enhance the user experience. I am
-              confident that my technical skills, passion for innovation, and
-              eagerness to learn will make me a valuable addition to your team.
-            </p>
-            <p>
-              Thank you for your time and consideration. I look forward to the
-              opportunity to discuss my qualifications further.
-            </p>
-            <p>Sincerely, Howard Chi</p>
+
+          <div className="faq__resume-real-container">
+            <h2 id="resume" className="faq__resume-title">
+              Resume
+            </h2>
+            <div className="faq__resume-container">
+              <img src={resume} className="faq__resume" alt="resume"></img>
+            </div>
+          </div>
+          <div className="faq__cover-letter-real-container">
+            <h2 id="cover-letter" className="faq__cover-letter">
+              Cover Letter
+            </h2>
+            <div className="faq__cover-letter-text">
+              <p>Dear Snap Engineering Academy,</p>
+              <p>
+                I am excited to submit my Website and potentionally move on to
+                the final round for a position at Snap Engineering Academy. As a
+                transfer student in Software Engineering, I have tried my best
+                to hone my skills in software engineering, programming, and data
+                analysis through hands-on experience and academic coursework. My
+                passion for innovation and technology has driven me to explore
+                the latest advancements in the field and learn new technologies
+                to stay current in this ever-evolving industry.
+              </p>
+              <p>
+                My previous work experience as a software engineer at John Deere
+                has given me the opportunity to develop and deliver successful
+                software solutions. I have gained proficiency in programming
+                languages such as Python, Java, and Web Developing languages. I
+                have also gained experience in agile development methodologies
+                and understand the importance of teamwork, communication, and
+                collaboration in achieving project goals.
+              </p>
+              <p>
+                I am drawn to Snapchat's commitment to creativity, innovation,
+                and the company's unique culture. I am excited to join the Snap
+                Engineering Academy team and contribute to developing
+                high-quality software solutions that enhance the user
+                experience. I am confident that my technical skills, passion for
+                innovation, and eagerness to learn will make me a valuable
+                addition to your team.
+              </p>
+              <p>
+                Thank you for your time and consideration. I look forward to the
+                opportunity to discuss my qualifications further.
+              </p>
+              <p>Sincerely, Howard Chi</p>
+            </div>
           </div>
         </div>
       </div>
