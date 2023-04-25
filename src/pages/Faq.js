3354676c001a152f6428b162f143ff07af1bdbb2
howardCLumi <102765211+howardCLumi@users.noreply.github.com>
2023-04-24 21:50:04 -0500

    fixed collasped

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -146,7 +146,6 @@ const Faq = () => {
           <a href="#form-questions">Form Questions</a>
           <a href="#resume-faq">Resume</a>
           <a href="#cover-letter">Cover Letter</a>
-          <a href="#test">test</a>
         </div>
       </div>
       <div className="faq">
@@ -171,6 +170,39 @@ const Faq = () => {
           <h2 id="cover-letter" className="faq__cover-letter">
             Cover Letter
           </h2>
+          <p>Dear Snap Engineering Academy,</p>
+          <p>
+            I am excited to submit my Website and move on to the final round for
+            a position at Snap Engineering Academy. As a transfer student in
+            Software Engineering, I have tried my best to hone my skills in
+            software engineering, programming, and data analysis through
+            hands-on experience and academic coursework. My passion for
+            innovation and technology has driven me to explore the latest
+            advancements in the field and learn new technologies to stay current
+            in this ever-evolving industry.
+          </p>
+          <p>
+            My previous work experience as a software engineer at John Deere has
+            given me the opportunity to develop and deliver successful software
+            solutions. I have gained proficiency in programming languages such
+            as Python, Java, and Web Developing languages. I have also gained
+            experience in agile development methodologies and understand the
+            importance of teamwork, communication, and collaboration in
+            achieving project goals.
+          </p>
+          <p>
+            I am drawn to Snapchat's commitment to creativity, innovation, and
+            the company's unique culture. I am excited to join the Snap
+            Engineering Academy team and contribute to developing high-quality
+            software solutions that enhance the user experience. I am confident
+            that my technical skills, passion for innovation, and eagerness to
+            learn will make me a valuable addition to your team.
+          </p>
+          <p>
+            Thank you for your time and consideration. I look forward to the
+            opportunity to discuss my qualifications further.
+          </p>
+          <p>Sincerely, Howard Chi</p>
         </div>
       </div>
     </>
